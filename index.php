2b1af3d639072cba75c642539d86deabbb130b56
Ben0214 <benihegedis@gmail.com>
2023-03-26 22:26:45 +0200

    Changes by Joshua Hegedus

--- index.php
+++ index.php
@@ -3,9 +3,6 @@ session_start();
 
 if(!isset($_SESSION["uname"]))
     echo "NOT LOGGED IN!!!";
-    echo "NOT LOGGED IN!!!";
-    echo "NOT LOGGED IN!!!";
-    echo "NOT LOGGED IN!!!";
 ?>
 
 <!DOCTYPE html>
